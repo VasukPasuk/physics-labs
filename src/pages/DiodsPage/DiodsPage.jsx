d5653a4a7760e0682817edeefc9ff423684ee3ec
VasukPasuk <142229155+VasukPasuk@users.noreply.github.com>
2024-05-10 18:13:16 +0300

    Add files via upload

--- src/pages/DiodsPage/DiodsPage.jsx
+++ src/pages/DiodsPage/DiodsPage.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './DiodsPage.style.scss';
 
+const invisible = {
+  opacity: '0'
+}
+
+const disperse = {
+  columnGap: `0.25rem`
+}
+
+const expand = {
+  columnGap: `1rem`
+}
+
 function DiodsPage(props) {
+  const [batteryState, setBatteryState] = useState(false); // false  + => - ; true - => +
+  const [start, setStart] = useState(false);
+  const [switched, setSwitched] = useState(false);
   return (
     <section className={`diods-page`}>
       <div className={`diods-box`}>
         <div className="p-field-wrapper field-wrapper">
-          <div className={`p-field field`}>
+          <div
+            className={`p-field field ${(start && batteryState) && 'move-left'}`}
+            style={(start && !batteryState) ? {} :disperse}
+          >
             {[...Array(25)].map(() => {
               return (
                 <div className={`circle hole-c`}>
@@ -23,10 +41,25 @@ function DiodsPage(props) {
           </div>
         </div>
         <div className="n-field-wrapper field-wrapper">
-          <div className={`n-field field`}>
+          <div
+            className={`n-field field ${(start && batteryState) && 'move-right'} ${(start && !batteryState) && 'moving'}`}
+            style={(start && !batteryState) ? {} : disperse}
+          >
             {[...Array(25)].map(() => {
               return (
-                <div className={`circle electron-c`}>
+                <div className={`circle electron-c ${(!start && !switched) && 'calm-state'}`}>
+                
+                </div>
+              )
+            })}
+          </div>
+          <div
+            className={`n-field field ${(start && batteryState) && 'move-right'} black-circles`}
+            style={(start && !batteryState) ? {} :disperse}
+          >
+            {[...Array(25)].map(() => {
+              return (
+                <div className={`circle electron-c black-circle'}`}>
                 
                 </div>
               )
@@ -39,8 +72,16 @@ function DiodsPage(props) {
             КАТОД
           </div>
         </div>
+        <div className="left-frame frame"
+             style={start ? {} : invisible}
+        />
+        <div className="right-frame frame"
+             style={start ? {} : invisible}
+        />
       </div>
-      <div className={`battery`}>
+      <div className={`battery ${(batteryState && start) && "active-rotation"}`}
+           style={start ? {} : invisible}
+      >
         <div className={`plus`}>
           +
         </div>
@@ -49,11 +90,30 @@ function DiodsPage(props) {
         </div>
       </div>
       <div className={`buttons-group`}>
-        <button className={`start-btn`}>
-          Старт
+        <button
+          className={`start-btn`}
+          onClick={() => {
+            setStart(true);
+          }}
+        >
+          Додати батарейку
         </button>
-        <button className={`toggle-btn`}>
+        <button
+          className={`toggle-btn`}
+          onClick={() => {
+            setBatteryState(prev => !prev);
+            setSwitched(prev => true)
+          }}
+        >
           Змінити полярність джерела струму
+        </button>
+        <button
+          className={`end-btn`}
+          onClick={() => {
+            setStart(false);
+          }}
+        >
+          Вилучити батарейку
         </button>
       </div>
     </section>
